3ed58e04dfbfe1ed94f1b81be3c0c536b6df9122
shivi35 <72295453+shivi35@users.noreply.github.com>
2021-06-18 10:28:07 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,25 @@
+class Paper{
+    constructor(x, y,radius) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0,
+            isStatic:true
+
+        }
+        this.body = Bodies.cirlce(x, y,radius, options);
+        this.radius =radius ;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse( 0, 0, this.radius, this.radius);
+        pop();
+      }
+}
+  
